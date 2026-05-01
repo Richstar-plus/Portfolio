3eed49d2c2b5956f7c632e4e1a8d97d01932dbdb
richard <richardsunday0812@gmail.com>
2026-05-01 11:01:20 +0100

    feat: enhance ContactPage with contact information cards and responsive styles; update About page styles for consistency

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,18 +1,57 @@
 import "./styles/Contact.css";
 import { useTheme } from "../hooks/useTheme";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPhone,
+  faEnvelope,
+  faMapMarkerAlt,
+} from "@fortawesome/free-solid-svg-icons";
 
 export function ContactPage() {
-  const { themColor } = useTheme();
+  const { themColor, openTheme, isNavOpen } = useTheme();
 
   return (
-    <div className="contact">
+    <div className={`contact ${isNavOpen ? "contact-content-holder-closed" : ""}`}>
       <h1 className="page-title">
         Get in <span className={`theme-color-${themColor}`}>Touch</span>
       </h1>
       <p className="contact-content">
         Feel free to reach out to me for any inquiries or collaborations.
       </p>
-      {/* Add your contact form here */}
+
+      <div className="contact-main-container">
+        <div className="contact-info">
+
+          <div className={`contact-info-card ${openTheme ? `contact-info-card-dark` : "contact-info-card-light"}`}>
+            <FontAwesomeIcon
+              icon={faPhone}
+              className={`contact-icon contact-icon-${themColor}`}
+            />
+            <h3>Phone</h3>
+            <p>+123 456 7890</p>
+          </div>
+
+          <div className={`contact-info-card ${openTheme ? `contact-info-card-dark` : "contact-info-card-light"}`}>
+            <FontAwesomeIcon
+              icon={faEnvelope}
+              className={`contact-icon contact-icon-${themColor}`}
+            />
+            <h3>Email</h3>
+            <p>example@example.com</p>
+          </div>
+
+          <div className={`contact-info-card ${openTheme ? `contact-info-card-dark` : "contact-info-card-light"}`}>
+            <FontAwesomeIcon
+              icon={faMapMarkerAlt}
+              className={`contact-icon contact-icon-${themColor}`}
+            />
+            <h3>Location</h3>
+            <p>123 Main Street, City, Country</p>
+          </div>
+
+        </div>
+        <div className="contact-form-container"></div>
+      </div>
     </div>
   );
 }

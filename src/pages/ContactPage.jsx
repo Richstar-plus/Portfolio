f14e23410036e6085414e742a9ae2115a6be5a7a
richard <richardsunday0812@gmail.com>
2026-05-01 15:35:55 +0100

    feat: enhance ContactPage layout and styles; add responsive form elements and improve overall design consistency

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -11,18 +11,25 @@ export function ContactPage() {
   const { themColor, openTheme, isNavOpen } = useTheme();
 
   return (
-    <div className={`contact ${isNavOpen ? "contact-content-holder-closed" : ""}`}>
-      <h1 className="page-title">
-        Get in <span className={`theme-color-${themColor}`}>Touch</span>
-      </h1>
+    <div
+      className={`contact ${isNavOpen ? "contact-content-holder-closed" : ""}`}
+    >
+      <div className="about-title">
+        <h1 className="page-title">
+          Get in <span className={`theme-color-${themColor}`}>Touch</span>
+        </h1>
+        <div className={`about-style1 border-theme-color-${themColor}`}></div>
+        <div className={`about-style2 border-theme-color-${themColor}`}></div>
+      </div>
       <p className="contact-content">
         Feel free to reach out to me for any inquiries or collaborations.
       </p>
 
       <div className="contact-main-container">
         <div className="contact-info">
-
-          <div className={`contact-info-card ${openTheme ? `contact-info-card-dark` : "contact-info-card-light"}`}>
+          <div
+            className={`contact-info-card ${openTheme ? `contact-info-card-dark` : "contact-info-card-light"}`}
+          >
             <FontAwesomeIcon
               icon={faPhone}
               className={`contact-icon contact-icon-${themColor}`}
@@ -31,7 +38,9 @@ export function ContactPage() {
             <p>+123 903 9014 066</p>
           </div>
 
-          <div className={`contact-info-card ${openTheme ? `contact-info-card-dark` : "contact-info-card-light"}`}>
+          <div
+            className={`contact-info-card ${openTheme ? `contact-info-card-dark` : "contact-info-card-light"}`}
+          >
             <FontAwesomeIcon
               icon={faEnvelope}
               className={`contact-icon contact-icon-${themColor}`}
@@ -40,7 +49,9 @@ export function ContactPage() {
             <p>richardsunday0812@gmail.com</p>
           </div>
 
-          <div className={`contact-info-card ${openTheme ? `contact-info-card-dark` : "contact-info-card-light"}`}>
+          <div
+            className={`contact-info-card ${openTheme ? `contact-info-card-dark` : "contact-info-card-light"}`}
+          >
             <FontAwesomeIcon
               icon={faMapMarkerAlt}
               className={`contact-icon contact-icon-${themColor}`}
@@ -48,13 +59,39 @@ export function ContactPage() {
             <h3>Location</h3>
             <p>123 Main Street, City, Country</p>
           </div>
-
         </div>
-        <div className="contact-form-container">
+        <div
+          className={`contact-form-container ${openTheme ? `contact-form-container-dark` : "contact-form-container-light"}`}
+        >
           <h2 className={`contact-form-title contact-form-title-${themColor}`}>
             SEND ME A MESSAGE
           </h2>
           <p>I am very responsive to messages.</p>
+          <div className="form">
+            <form className="contact-form">
+              <div className="input-container">
+                <div className="input">
+                  <input type="text" placeholder="Your Name" required />
+                  <input type="email" placeholder="Your Email" required />
+                </div>
+                <div className="input-subject">
+                  <input type="text" placeholder="Subject" required />
+                </div>
+                <div className="input-text">
+                  <textarea
+                    placeholder="Your Message"
+                    required
+                    rows="5"
+                  ></textarea>
+                </div>
+              </div>
+              <button
+                className={`contact-form-button contact-form-button-${themColor}`}
+              >
+                Send Message
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>

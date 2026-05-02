bc97b4b65feaaecb591cd26e5c9b149b040937c5
richard <richardsunday0812@gmail.com>
2026-05-02 12:48:21 +0100

    style: improve formatting of experience details in Experience component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -16,7 +16,9 @@ export function Experience({ title, openTheme, themColor }) {
             key={experience.id}
             className={`experience-details experience-border-theme-color-${themColor}`}
           >
-            <div className={`experience-style experience-style-${themColor}`}></div>
+            <div
+              className={`experience-style experience-style-${themColor}`}
+            ></div>
             <div className="experience-content">
               <p className="date">{experience.date}</p>
               <p className="experience-category">{experience.category}</p>
